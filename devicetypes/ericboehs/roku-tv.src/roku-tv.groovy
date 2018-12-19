6c8b51aaa360b921024e5a2e2d988e01b2db74b7
JOSE PEREZ <jrbandido@hotmail.com>
2018-12-18 22:11:58 -0500

    Done for the night

--- devicetypes/ericboehs/roku-tv.src/roku-tv.groovy
+++ devicetypes/ericboehs/roku-tv.src/roku-tv.groovy
@@ -17,9 +17,13 @@ metadata {
     command "right"
     command "volume_up"
     command "volume_down"
+    command "channel_up"
+    command "channel_down"
     command "volume_mute"
     command "netflix"
     command "plex"
+    command "youtube"
+    command "primevideo"
     command "hdmi1"
     command "hdmi2"
     command "hdmi3"
@@ -30,86 +34,102 @@ metadata {
   simulator {
   }
 
-  tiles {
-    standardTile("button", "device.switch", width: 1, height: 1, canChangeIcon: true) {
+  tiles(scale: 2) {
+    standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
       state "off", label: 'Off', action: "switch.on", icon: "sst.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
       state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
     }
 
-    standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh-icon"
     }
 
-	standardTile("home", "device.switch", inactiveLabel: false, decoration: "flat") {
+	standardTile("home", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Home", action:"home", icon:"https://static.helpjuice.com/helpjuice_production/uploads/upload/image/2771/114109/home1600.png"
     }
     
-    	standardTile("back", "device.switch", inactiveLabel: false, decoration: "flat") {
+    	standardTile("back", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Back", action:"back", icon:"https://cdn4.iconfinder.com/data/icons/layout-control-icons/100/14-512.png"
     }
 
-	standardTile("up", "device.switch", inactiveLabel: false, decoration: "flat") {
+	standardTile("up", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Up", action:"up", icon:"st.thermostat.thermostat-up"
     }
     
-    	standardTile("down", "device.switch", inactiveLabel: false, decoration: "flat") {
+    	standardTile("down", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Down", action:"down", icon:"st.thermostat.thermostat-down"
     }
     
-    	standardTile("left", "device.switch", inactiveLabel: false, decoration: "flat") {
+    	standardTile("left", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Left", action:"left", icon:"st.thermostat.thermostat-left"
     }
     
-    	standardTile("right", "device.switch", inactiveLabel: false, decoration: "flat") {
+    	standardTile("right", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Right", action:"right", icon:"st.thermostat.thermostat-right"
     }
     
-      	standardTile("select", "device.switch", inactiveLabel: false, decoration: "flat") {
+      	standardTile("select", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"OK", action:"select", icon:"st.illuminance.illuminance.dark"
     }
     
-    standardTile("volume_up", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("volume_up", "device.switch", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Vol +", action:"volume_up", icon:"st.custom.sonos.unmuted"
     }
 
-    standardTile("volume_down", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("volume_down", "device.switch", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Vol -", action:"volume_down", icon:"st.custom.sonos.unmuted"
     }
+    
+        standardTile("channel_up", "device.switch", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
+      state "default", label:"CH +", action:"channel_up", icon:"st.custom.sonos.unmuted"
+    }
 
-    standardTile("volume_mute", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("channel_down", "device.switch", width: 2, height: 1, inactiveLabel: false, decoration: "flat") {
+      state "default", label:"CH -", action:"channel_down", icon:"st.custom.sonos.unmuted"
+    }
+
+    standardTile("volume_mute", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Mute", action:"volume_mute", icon:"st.custom.sonos.muted"
     }
 
-    standardTile("netflix", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("netflix", "device.switch", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Netflix", action:"netflix", icon:"http://icons.iconarchive.com/icons/stalker018/mmii-flat-vol-2/128/netflix-icon.png"
     }
 
-    standardTile("plex", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("plex", "device.switch", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Plex", action:"plex", icon:"https://wiki.mrmc.tv/images/c/cf/Plex_icon.png"
     }
+    
+     standardTile("youtube", "device.switch", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
+      state "default", label:"Youtube", action:"youtube", icon:"http://pngimg.com/uploads/youtube/youtube_PNG18.png"
+    }
 
-    standardTile("hdmi1", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("amazonvideo", "device.switch", width: 1, height: 1, inactiveLabel: false, decoration: "flat") {
+      state "default", label:"Amazon Video", action:"amazonvideo", icon:"https://images-na.ssl-images-amazon.com/images/I/411j1k1u9yL._SY355_.png"
+    }
+
+    standardTile("hdmi1", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"HDMI 1", action:"hdmi1", icon:"st.Electronics.electronics12"
     }
 
-    standardTile("hdmi2", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("hdmi2", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"HDMI 2", action:"hdmi2", icon:"st.Electronics.electronics12"
     }
 
-    standardTile("hdmi3", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("hdmi3", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"HDMI 3", action:"hdmi3", icon:"st.Electronics.electronics12"
     }
 
-    standardTile("cvbs", "device.switch", inactiveLabel: false, decoration: "flat") {
+    standardTile("cvbs", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"AV", action:"cvbs", icon:"st.Electronics.electronics12"
     }
     
-        standardTile("antenna", "device.switch", inactiveLabel: false, decoration: "flat") {
+        standardTile("antenna", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
       state "default", label:"Antenna", action:"antenna", icon:"st.Electronics.electronics12"
     }
 
     main "button"
-    details(["back", "button", "home", "volume_down", "up", "volume_up", "left", "select",  "right", "netflix",  "down", "plex", "hdmi1", "hdmi2", "hdmi3", "cvbs", "antenna", "refresh","volume_mute",])
+    details(["back", "button", "home", "volume_up","up", "channel_up",  "volume_down","channel_down","left",     "select","right","netflix", "plex",  "down", "hdmi1", "youtube", "amazonvideo", "hdmi2", "hdmi3", "cvbs", "antenna", "refresh","volume_mute",])
   }
 }
 
@@ -127,6 +147,8 @@ def parse(String description) {
   def msg = parseLanMessage(description)
   if (msg.body && msg.body.contains("PowerOn")) {
     sendEvent(name: "switch", value: "on")
+  }else{
+    sendEvent(name: "switch", value: "off")
   }
 }
 
@@ -185,6 +207,14 @@ def volume_down() {
   keypress('VolumeDown')
 }
 
+def channel_up() {
+  keypress('ChannelUp')
+}
+
+def channel_down() {
+  keypress('ChannelDown')
+}
+
 def volume_mute() {
   keypress('VolumeMute')
 }
@@ -197,6 +227,13 @@ def plex() {
   launchApp('13535')
 }
 
+def youtube() {
+  launchApp('837')
+}
+
+def amazonvideo() {
+  launchApp('13')
+}
 
 def antenna() {
   keypress('InputTuner')
@@ -229,7 +266,7 @@ def refresh() {
 }
 
 def queryDeviceState() {
-  sendEvent(name: "switch", value: "off")
+  
   sendHubCommand( new physicalgraph.device.HubAction(
     method: "GET",
     path: "/query/device-info",
